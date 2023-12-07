01268668bec0f851601a33b28c677a29616d2f30
Jordan <jordanwatson000@gmail.com>
2023-12-07 21:36:58 +0000

    add additional tasks on readme. explored modifiers. hdr environment added

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Testing-Three-js
 
 # Introduction
+
 This is a playground for my own development of using 3D Blender files in React/three.js
 After a career in furniture/interior design this is a way to merge previous modelling skills into the great world of React and have some fun with it!
 
--Jordan
+-Jordan 🐛
+
+# Personal tasks
+
+-control lighting of starting scene  ✅
+-acquire modifiers of imported mesh (could require applying modifiers prior to export?) ✅
+-add HDRI to a scene ✅
+-create a background/plane for context
+-allow controls in a scene
+-resize the scene to a contained div or element on the window
+-split the file into react components
+-overlay basic html to simulate a menu
+-create a clickable response to a 3D object
+-allow the import/control of cameras from Blender
+-allow the import/control of lighting from Blender accurately
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import WebGL from 'three/addons/capabilities/WebGL.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
+//project setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -19,11 +18,26 @@ const renderer = new THREE.WebGL1Renderer({
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(3);
 renderer.render(scene, camera);
 
-const loader = new GLTFLoader();
+//lights
+const ambientlight = new THREE.AmbientLight(0x404040, 30);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+scene.add(ambientlight);
 
+//hdri
+new RGBELoader()
+  .load('../assets/netball_court_4k.hdr', function (texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+    render()
+  });
+
+//import blender file
+const loader = new GLTFLoader();
 loader.load(
   '../assets/test3Dfile.glb',
   function (gltf) {
