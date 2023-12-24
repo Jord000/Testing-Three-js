f6cf6317c17032721f19b659590b93eb566dd2f3
Jordan <jordanwatson000@gmail.com>
2023-12-24 16:25:35 +0000

    added menu pop up on click to flubber in more detail

--- src/FlubberMenu.jsx
+++ src/FlubberMenu.jsx
@@ -2,7 +2,7 @@ const FlubberMenu = () => {
   return (
     <div className="flubber-menu-container">
       <img src="https://m.media-amazon.com/images/M/MV5BZDlkOTA0OTUtOThmMi00NTMyLTg0NDgtZWZiYmRjZWI4MDY4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg" />
-      <p>https://www.imdb.com/title/tt0119137/</p>
+      <p><a href="https://www.imdb.com/title/tt0119137/" target="_blank" rel="noopener noreferrer">Let's go see flubber at IMDB</a></p>
     </div>
   )
 }
